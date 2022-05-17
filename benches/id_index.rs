e571c2c0e01f093f07a35e143ecd0f9731745679
Fey Martynov <feymartynov@gmail.com>
2022-05-18 00:28:04 +0300

    Add parking_lot rwlock to benchmark

--- benches/id_index.rs
+++ benches/id_index.rs
@@ -10,13 +10,14 @@ use std::collections::hash_map::DefaultHasher;
 use std::collections::{BTreeMap, HashMap};
 use std::hash::{BuildHasherDefault, Hasher};
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, RwLock as StdRwLock};
 use std::thread;
 use std::time::Duration;
 
 use bencher::Bencher;
 use lockfree::map::Map as LockFreeMap;
 use nohash_hasher::NoHashHasher;
+use parking_lot::RwLock as ParkingLotRwLock;
 use rand::prelude::*;
 use rustc_hash::FxHasher;
 
@@ -41,7 +42,7 @@ struct RwLockBTreeMapUpdater {
 }
 
 impl RwLockBTreeMapUpdater {
-    fn start(ids: Arc<RwLock<BTreeMap<Id, usize>>>) -> Self {
+    fn start(ids: Arc<StdRwLock<BTreeMap<Id, usize>>>) -> Self {
         let is_halt = Arc::new(AtomicBool::new(false));
         let is_halt_clone = is_halt.clone();
 
@@ -72,7 +73,7 @@ fn id_index_rwlock_btree_map(bencher: &mut Bencher) {
         ids.insert(id, 0);
     }
 
-    let ids = Arc::new(RwLock::new(ids));
+    let ids = Arc::new(StdRwLock::new(ids));
     let _updater = RwLockBTreeMapUpdater::start(ids.clone());
     let mut rng = rand::thread_rng();
 
@@ -86,12 +87,12 @@ fn id_index_rwlock_btree_map(bencher: &mut Bencher) {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-struct RwLockHashUpdater {
+struct StdRwLockHashUpdater {
     is_halt: Arc<AtomicBool>,
 }
 
-impl RwLockHashUpdater {
-    fn start<H>(ids: Arc<RwLock<HashMap<Id, usize, BuildHasherDefault<H>>>>) -> Self
+impl StdRwLockHashUpdater {
+    fn start<H>(ids: Arc<StdRwLock<HashMap<Id, usize, BuildHasherDefault<H>>>>) -> Self
     where
         H: Hasher + Default + 'static,
     {
@@ -112,13 +113,13 @@ impl RwLockHashUpdater {
     }
 }
 
-impl Drop for RwLockHashUpdater {
+impl Drop for StdRwLockHashUpdater {
     fn drop(&mut self) {
         self.is_halt.store(true, Ordering::SeqCst);
     }
 }
 
-fn id_index_rwlock_hash<H: Hasher + Default + 'static>(bencher: &mut Bencher) {
+fn id_index_std_rwlock_hash<H: Hasher + Default + 'static>(bencher: &mut Bencher) {
     let hasher = BuildHasherDefault::<H>::default();
     let mut ids = HashMap::with_capacity_and_hasher(SIZE, hasher);
 
@@ -126,14 +127,68 @@ fn id_index_rwlock_hash<H: Hasher + Default + 'static>(bencher: &mut Bencher) {
         ids.insert(id, 0);
     }
 
-    let ids = Arc::new(RwLock::new(ids));
-    let _updater = RwLockHashUpdater::start(ids.clone());
+    let ids = Arc::new(StdRwLock::new(ids));
+    let _updater = StdRwLockHashUpdater::start(ids.clone());
     let mut rng = rand::thread_rng();
 
     bencher.iter(|| {
         for _ in 0..READS_PER_BENCH_ITER {
             let id = rng.gen_range(1..(SIZE as Id));
             prevent_opt(ids.read().expect("Failed to get read lock").get(&id));
+        }
+    })
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+struct ParkingLotRwLockHashUpdater {
+    is_halt: Arc<AtomicBool>,
+}
+
+impl ParkingLotRwLockHashUpdater {
+    fn start<H>(ids: Arc<ParkingLotRwLock<HashMap<Id, usize, BuildHasherDefault<H>>>>) -> Self
+    where
+        H: Hasher + Default + 'static,
+    {
+        let is_halt = Arc::new(AtomicBool::new(false));
+        let is_halt_clone = is_halt.clone();
+
+        thread::spawn(move || {
+            let mut rng = rand::thread_rng();
+
+            while !is_halt_clone.load(Ordering::Relaxed) {
+                let id = rng.gen_range(LAST_FILLED_ID..(SIZE as Id));
+                ids.write().insert(id, 0);
+                thread::sleep(Duration::from_millis(UPDATER_PERIOD_MS));
+            }
+        });
+
+        Self { is_halt }
+    }
+}
+
+impl Drop for ParkingLotRwLockHashUpdater {
+    fn drop(&mut self) {
+        self.is_halt.store(true, Ordering::SeqCst);
+    }
+}
+
+fn id_index_parking_lot_rwlock_hash<H: Hasher + Default + 'static>(bencher: &mut Bencher) {
+    let hasher = BuildHasherDefault::<H>::default();
+    let mut ids = HashMap::with_capacity_and_hasher(SIZE, hasher);
+
+    for id in 0..LAST_FILLED_ID {
+        ids.insert(id, 0);
+    }
+
+    let ids = Arc::new(ParkingLotRwLock::new(ids));
+    let _updater = ParkingLotRwLockHashUpdater::start(ids.clone());
+    let mut rng = rand::thread_rng();
+
+    bencher.iter(|| {
+        for _ in 0..READS_PER_BENCH_ITER {
+            let id = rng.gen_range(1..(SIZE as Id));
+            prevent_opt(ids.read().get(&id));
         }
     })
 }
@@ -196,9 +251,12 @@ fn id_index_lock_free_map<H: Hasher + Default + 'static>(bencher: &mut Bencher) 
 benchmark_group!(
     benches,
     id_index_rwlock_btree_map,
-    id_index_rwlock_hash::<DefaultHasher>,
-    id_index_rwlock_hash::<FxHasher>,
-    id_index_rwlock_hash::<NoHashHasher<Id>>,
+    id_index_std_rwlock_hash::<DefaultHasher>,
+    id_index_std_rwlock_hash::<FxHasher>,
+    id_index_std_rwlock_hash::<NoHashHasher<Id>>,
+    id_index_parking_lot_rwlock_hash::<DefaultHasher>,
+    id_index_parking_lot_rwlock_hash::<FxHasher>,
+    id_index_parking_lot_rwlock_hash::<NoHashHasher<Id>>,
     id_index_lock_free_map::<DefaultHasher>,
     id_index_lock_free_map::<FxHasher>,
     id_index_lock_free_map::<NoHashHasher<Id>>,
